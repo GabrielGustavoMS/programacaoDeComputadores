15b1ef5a90ff7d1fa666dc43391eeab6f2fba8b3
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 22:20:02 -0300

    Add files via upload

---
+++ outros/media.py
@@ -0,0 +1,8 @@
+n1 = float(input("Entre com o primeiro valor: "))
+n2 = float(input("Entre com o segundo valor: "))
+n3 = float(input("Entre com o terceiro valor: "))
+
+media = (n1+n2+n3)/3
+
+print("O valor da média vale", media, "!")
+
