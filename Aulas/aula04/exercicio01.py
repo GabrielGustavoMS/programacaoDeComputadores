a774306fd1e1c19c0c7ff40390c1a9259b71f489
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 22:30:34 -0300

    Add files via upload

---
+++ Aulas/aula04/exercicio01.py
@@ -0,0 +1,7 @@
+n1 = float(input("Digite o primeiro valor:   "))
+n2= float(input("Digite o 2º valor:   "))
+n3= float(input("Digite o 3º valor:   "))
+
+m = (n1+n2+n3)/3
+print("A média é :  ", m)
+
