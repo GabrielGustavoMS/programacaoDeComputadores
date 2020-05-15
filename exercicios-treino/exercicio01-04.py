d5daa670849b2ee8903f146e8734907c9d0c5cd1
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 21:26:34 -0300

    Add files via upload

---
+++ exercicios-treino/exercicio01-04.py
@@ -0,0 +1,5 @@
+n1 = float(input('Digite no 1º número  '))
+n2 = float(input('Digite no 2º número  '))
+n3 = float(input('Digite no 3º número   '))
+m= (n1+n2+n3)/3
+print('A média entre os 3 valores é :   ', m)
