360316da8fcb837defb3e61c05cb2288fb201db1
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 22:29:41 -0300

    Add files via upload

---
+++ Aulas/aula04/aula04.py
@@ -0,0 +1,7 @@
+q25 = input("Entre com o valor de moedas de R$ 0,25 =")
+q10 = input("Entre com o valor de moedas de R$ 0,10=")
+q5 = input("Entre com o valor de moedas de R$ 0,05=")
+
+total = int(q25)*0.25+int(q10)*0.10+int(q5)*0.05
+
+print("O valor total em reais é:R$",total)
