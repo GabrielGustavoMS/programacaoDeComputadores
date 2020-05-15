7ea2e3f26916e81e06294b07681003cd449d1641
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 22:19:40 -0300

    Add files via upload

---
+++ outros/maior(1).py
@@ -0,0 +1,10 @@
+n1 = int(input("Primeiro número: "))
+n2 = int(input("Segundo número: "))
+n3 = int(input("Terceiro número: "))
+
+if n1>n2 and n1>n3:
+    print(n1)
+elif n2>n3:
+    print(n2)
+else:
+    print(n3)
