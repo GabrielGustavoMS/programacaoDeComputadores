fb07dd73bd4bfdccae643b234fb94b87d69fef3c
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 21:26:03 -0300

    Add files via upload

---
+++ exercicios-treino/exemplo1-04.py
@@ -0,0 +1,7 @@
+m25 = int(input('Digite a o nº de moedas de R$0,25:  '))
+m10 = int(input('Digite a o nº de moedas de R$0,10:  '))
+m5 = int(input('Digite a o nº de moedas de R$0,05:  '))
+
+cofre = m25*0.25+m10*0.1+m5*0.05
+
+print('A o valor de moedas do cofre é:   ', cofre)
