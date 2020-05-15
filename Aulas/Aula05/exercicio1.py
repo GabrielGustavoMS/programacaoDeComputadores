e12e46b2bfd316f1edc467d087b81bfd62c8326f
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 22:33:20 -0300

    Add files via upload

---
+++ Aulas/Aula05/exercicio1.py
@@ -0,0 +1,17 @@
+numb = int(input(" Digite um número de 0 à 100: " ))
+
+if numb > 100:
+    print("Valor inválido:  ")
+else:
+    if numb%3==0:
+        print("O valor é múltiplo de 3")
+    else:
+        print("O valor não é múltiplo: ")
+
+        
+  
+
+    
+
+    
+    
