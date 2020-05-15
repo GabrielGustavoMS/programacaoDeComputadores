8106266c64e92fcd61ff2a3277e1db7639e6e838
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 21:26:19 -0300

    Add files via upload

---
+++ exercicios-treino/exemplo2-04.py
@@ -0,0 +1,4 @@
+salario = float(input('Digite o salário: ==> '))
+aumento = float(input('Digite o valor da porcentagem do aumento:   '))
+salario = salario + (salario*aumento)/100
+print('O salario com o aumento é:   ', salario)
