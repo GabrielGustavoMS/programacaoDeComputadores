18a6cef135eab3100e465801659da37161b22f01
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 22:19:15 -0300

    Add files via upload

---
+++ outros/imc.py
@@ -0,0 +1,8 @@
+peso = float(input("Entre com o seu peso sem mentir: "))
+altura = float(input("Entre com a sua altura em metros: "))
+
+imc = peso / altura**2
+
+print("O seu IMC vale", imc)
+
+      
