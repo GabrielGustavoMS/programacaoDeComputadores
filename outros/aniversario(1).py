573851cd560ac1bd0f627ef72455dafbf6a5bb0b
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 22:17:56 -0300

    Add files via upload

---
+++ outros/aniversario(1).py
@@ -0,0 +1,33 @@
+dia = int(input("Qual o dia do niver: "))
+mes = int(input("Qual o mês do niver: "))
+
+if mes==1:
+    nome = "Janeiro"
+elif mes==2:
+    nome = "Fevereiro"
+elif mes==3:
+    nome = "Março"
+elif mes==4:
+    nome = "Abril"
+elif mes==5:
+    nome = "Maio"
+elif mes==6:
+    nome = "Junho"
+elif mes==7:
+    nome = "Julho"
+elif mes==8:
+    nome = "Agosto"
+elif mes==9:
+    nome = "Setembro"
+elif mes==10:
+    nome = "Outubro"
+elif mes==11:
+    nome = "Novembro"
+elif mes==12:
+    nome = "Dezembro"
+
+print("Data do seu aniversário é", dia, "de", nome
+
+
+
+
