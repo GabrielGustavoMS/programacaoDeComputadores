2947ec9a0f49585dd2ac7363cfd83ba1c627e06b
GabrielGustavoMS <55142297+GabrielGustavoMS@users.noreply.github.com>
2020-05-14 21:28:28 -0300

    Add files via upload

---
+++ exercicios-treino/exercicio03-03.py
@@ -0,0 +1,7 @@
+nome = input('Digite o nome do aluno ==> ')
+nota1 = float(input('Digite a 1º nota:   '))
+nota2 = float(input('Digite a 2º nota:   '))
+
+m =(nota1+nota2)/2
+
+print('A média aritimética entre as notas é :    ', m)
